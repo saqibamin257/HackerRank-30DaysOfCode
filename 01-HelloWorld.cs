e7cf5d3e162d5b175d762043868ca6a8e35878cd
Saqib Amin <saqibamin257@gmail.com>
2021-07-06 06:39:22 -0700

    Add project files.

---
+++ 01-HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank_30DaysOfCode
+{
+  public class HelloWorld
+    {
+        public void PrintMessage() 
+        {
+            string inputString;
+            inputString = Console.ReadLine();
+            Console.WriteLine("Hello World");
+            Console.WriteLine(inputString);
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank_30DaysOfCode
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld.PrintMessage();
+        }
+    }
+}
